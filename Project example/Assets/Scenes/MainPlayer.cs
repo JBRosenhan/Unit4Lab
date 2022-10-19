f8391beb7586b87acfefc50f7a9fc983326517e3
Jace Bradly Rosenhan <jace.b.rosenhan@gmail.com>
2022-10-19 13:35:44 -0600

    Practice

---
+++ Project example/Assets/Scenes/MainPlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainPlayer : MonoBehaviour
+{
+    public string myName;
+
+    
+    void Start ()
+    {
+        Debug.Log("I am alive");
+    }
+    
+    
+    void Update () {
+    
+    }
+}
