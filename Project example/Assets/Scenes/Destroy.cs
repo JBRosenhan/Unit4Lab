f1b59cefc1ee1c1ab3d4d7e542dd08605e0bf945
Jace Bradly Rosenhan <jace.b.rosenhan@gmail.com>
2022-10-19 18:55:17 -0600

    Practiced Destroy

---
+++ Project example/Assets/Scenes/Destroy.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class DestroyBasic : MonoBehaviour
+{
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+public class DestroyOther : MonoBehaviour
+{
+    public GameObject other;
+    
+    
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(other);
+        }
+    }
+}
+
+public class DestroyComponent : MonoBehaviour
+{
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Destroy(GetComponent<MeshRenderer>());
+        }
+    }
+}
